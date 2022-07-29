1385e315adf2f31182bb3dc5585821f7ec266d6a
Gabrieloliveiramp3 <gabrieloliveira3500@gmail.com>
2022-07-29 13:02:29 -0300

    Update config

--- main.py
+++ main.py
@@ -21,16 +21,31 @@ def home():
 def listar_filmes():
     return banco
 
-@app.get('/search/{movies_id}')
+@app.get('/movies/{movies_id}')
 def procurar_filmes(movies_id: str):
     for movie in banco:
         if movie.Id == movies_id:
             return movie
 
+@app.delete('/movies/{movies_id}')
+def remover_filmes(movies_id: str):
+    posicao = -1
+    #Busca a posição do filme
+    for index, movie in enumerate(banco):
+        if movie.Id == movies_id:
+            posicao = index
+            break
 
-@app.post("/cadmovies")
+    if posicao != -1:
+        banco.pop(posicao)
+        return {'Filme removido com sucesso'}
+    else:
+        return {'Filme não localizado'}
+
+@app.post("/movies")
 def cadastrar_filmes(filme: Movies):
     banco.append(filme)
+    #uuid4 gera o id automaticamente
     filme.Id = str(uuid4())
     return {f"Filme {filme.Titulo} cadastrado com sucesso!"}
 
